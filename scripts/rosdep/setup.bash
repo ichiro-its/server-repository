542c6e1e6d7c952936b850b0dab46e8b1fc85400
maroqijalil <jalilpro31@gmail.com>
2022-02-02 17:30:08 +0700

    feat: add rosdep loading script

--- scripts/rosdep/setup.bash
+++ scripts/rosdep/setup.bash
@@ -2,3 +2,5 @@
 
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=x864] http://packages.ros.org/ros2/ubuntu main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo sh -c 'yaml https://repository.ichiro-its.org/rosdep/debian-packages.yaml" > repository.ichiro-its.org/debian/ichiro-its-packages.list'
