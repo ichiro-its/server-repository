02939ceac914da31d15cc235a3b3fc4174bb944f
maroqijalil <jalilpro31@gmail.com>
2022-02-03 10:46:33 +0700

    refactor: add echo comand to rosdep script

--- scripts/rosdep/setup.bash
+++ scripts/rosdep/setup.bash
@@ -3,4 +3,4 @@
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=x864] http://packages.ros.org/ros2/ubuntu main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo sh -c 'yaml "https://repository.ichiro-its.org/rosdep/debian-packages.yaml" > /etc/ros/rosdep/sources.list.d/ichiro-its-packages.list'
+sudo sh -c 'echo "yaml https://repository.ichiro-its.org/rosdep/debian-packages.yaml" > /etc/ros/rosdep/sources.list.d/ichiro-its-packages.list'
