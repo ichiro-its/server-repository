0ce35adf9aab379072e4fb94d651849560783b7d
M Maroqi Abdul Jalil <jalilpro31@gmail.com>
2022-08-14 23:23:15 +0700

    refactor: change directory target to get from args

--- script.bash
+++ script.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DIR="/var/www/repository.ichiro-its.org"
+[ -z "$1" ] && printf "Please, specify the target directory!\nUsage: bash script.sh [target directory]"
+
+DIR="$1"
 
 bash ./scripts/generate-key.bash
 bash ./scripts/configure-reprepro.bash "$DIR"
