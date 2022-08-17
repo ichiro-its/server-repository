06e88d7b2bd63390b6d5c288f38ce62cf26cddac
maroqijalil <jalilpro31@gmail.com>
2022-08-17 11:58:31 +0700

    refactor: adjust script to be more dynamic

--- script.bash
+++ script.bash
@@ -2,12 +2,19 @@
 
 if [ -z "$1" ]
 then
-  printf "Please, specify the target directory!\nUsage: bash script.sh [target directory]\n"
+  printf "Please, specify the target directory!\nUsage: bash script.sh [target directory] [domain]\n"
+  exit 1
+fi
+
+if [ -z "$2" ]
+then
+  printf "Please, specify the domain!\nUsage: bash script.sh [target directory] [domain]\n"
   exit 1
 fi
 
 DIR="$1"
+DOMAIN="$2"
 
 bash ./scripts/generate-key.bash
 bash ./scripts/configure-reprepro.bash "$DIR"
-bash ./scripts/include-scripts.bash "$DIR"
+bash ./scripts/include-scripts.bash "$DIR" "$DOMAIN"
--- scripts/include-scripts.bash
+++ scripts/include-scripts.bash
@@ -3,8 +3,33 @@
 DEBIAN_DIR="$1/debian"
 ROSDEP_DIR="$1/rosdep"
 
-cp ./scripts/debian/*.bash $DEBIAN_DIR
+cat << EOF > "$DEBIAN_DIR/setup-nightly.bash"
+#!/bin/bash
+
+wget -O - http://$2/conf/ichiro.gpg.key | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://$2/debian nightly main" > /etc/apt/sources.list.d/ichiro.list'
+EOF
+
+cat << EOF > "$DEBIAN_DIR/setup.bash"
+#!/bin/bash
+
+wget -O - http://$2/conf/ichiro.gpg.key | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://$2/debian stable main" > /etc/apt/sources.list.d/ichiro.list'
+EOF
 
 mkdir -p $ROSDEP_DIR
-cp ./scripts/rosdep/*.bash $ROSDEP_DIR
+
+cat << EOF > "$ROSDEP_DIR/setup.bash"
+#!/bin/bash
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=x864] http://packages.ros.org/ros2/ubuntu main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+if [ ! -d "/etc/ros/rosdep/sources.list.d/" ]; then
+  sudo mkdir -p "/etc/ros/rosdep/sources.list.d/"
+fi
+
+sudo sh -c 'echo "yaml https://$2/rosdep/packages.yaml" > /etc/ros/rosdep/sources.list.d/ichiro.list'
+EOF
+
 cp ./data/*.yaml $ROSDEP_DIR
