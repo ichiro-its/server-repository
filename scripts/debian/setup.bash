e11a7c072bb4e1c2664a4634acb491c90749b146
maroqijalil <jalilpro31@gmail.com>
2022-08-17 13:26:55 +0700

    refactor: remove unused scripts since that will be generated

--- scripts/debian/setup.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget -O - https://repository.ichiro-its.org/conf/ichiro.gpg.key | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://repository.ichiro-its.org/debian stable main" > /etc/apt/sources.list.d/ichiro.list'
