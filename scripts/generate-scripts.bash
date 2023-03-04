18463e5240fd180b96fa237523a786b80a7d36c1
borednuna <hanunshaka02@gmail.com>
2023-03-04 19:47:37 +0700

    fix: fix setup sources

--- scripts/generate-scripts.bash
+++ scripts/generate-scripts.bash
@@ -23,8 +23,9 @@ mkdir -p $ROSDEP_DIR
 cat > "$ROSDEP_DIR/setup.bash" <<EOL
 #!/bin/bash
 
+sudo apt update && sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=x864] http://packages.ros.org/ros2/ubuntu main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 if [ ! -d "/etc/ros/rosdep/sources.list.d/" ]; then
   sudo mkdir -p "/etc/ros/rosdep/sources.list.d/"
